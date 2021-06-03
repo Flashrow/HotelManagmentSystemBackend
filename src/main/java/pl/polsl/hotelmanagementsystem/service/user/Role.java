fd1bb9ad74400aa9e2f4640c4668825e6a3b09df
michalpatryk <43810475+michalpatryk@users.noreply.github.com>
2021-06-03 13:14:20 +0200

    Removed ROLE_QUEST from Role

--- src/main/java/pl/polsl/hotelmanagementsystem/service/user/Role.java
+++ src/main/java/pl/polsl/hotelmanagementsystem/service/user/Role.java
@@ -4,7 +4,7 @@ import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
     ROLE_ADMIN, ROLE_CLIENT, ROLE_STAFF,
-    ROLE_MANAGER, ROLE_GUEST, ROLE_KITCHEN, ROLE_ROOM_SERVICE, ROLE_RECEPTION;
+    ROLE_MANAGER, ROLE_KITCHEN, ROLE_ROOM_SERVICE, ROLE_RECEPTION;
     public String getAuthority(){
         return name();
     }
